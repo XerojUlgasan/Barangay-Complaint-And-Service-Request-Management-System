6f41c017cddecc158dd1e162c1b050e84abd72be
Xeroj Nilles Ulgasan <xerojulgasan@gmail.com>
2026-02-09 14:42:32 +0800

    first commit

---
+++ src/supabse_db/supabase_client.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  "https://qcnljiogxnmfugcaqxge.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjbmxqaW9neG5tZnVnY2FxeGdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0NjM1NTAsImV4cCI6MjA4NjAzOTU1MH0.C_GLCdO2YjmHMz4UAnSnMxMIjVVwIO8I3tVFGrgBSZc",
+);
+
+export default supabase;
